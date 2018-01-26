f36f25c60504bfafc390cfa0ec6f56083df94549
David Bauer <bauer.david@att.net>
2018-01-25 20:14:20 -0500

    modified routes/htmlRouter.js

--- routes/htmlRouter.js
+++ routes/htmlRouter.js
@@ -2,14 +2,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html")); // Try this: ../13-StarWars-6/view2.html
-});
+module.exports = function(app) {
+	app.get("/", function(req, res) {
+	  res.sendFile(path.join(__dirname, "index.html")); // Try this: ../13-StarWars-6/view2.html
+	});
 
-app.get("/view", function(req, res) {
-  res.sendFile(path.join(__dirname, "viewTable.html"));
-});
+	app.get("/view", function(req, res) {
+		res.sendFile(path.join(__dirname, "viewTable.html"));
+	});
 
-app.get("/reservations", function(req, res) {
-  res.sendFile(path.join(__dirname, "reservation.html"));
-});
+	app.get("/reservations", function(req, res) {
+		res.sendFile(path.join(__dirname, "reservation.html"));
+	});
+}
+
